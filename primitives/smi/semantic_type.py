9ad8e3575e47adc52c4c79c8cef213108b0f23e4
tjoseph <tjoseph.ubc@gmail.com>
2020-01-30 18:10:41 -0800

    updated

--- primitives/smi/semantic_type.py
+++ primitives/smi/semantic_type.py
@@ -166,7 +166,7 @@ class SemanticTypeInfer(transformer.TransformerPrimitiveBase[Inputs, Outputs, Hy
                    ('https://dl.dropboxusercontent.com/s/e87v5m92nktior7/par_vec_trained_400.pkl?dl=1'),
                    '6b4f0ace998ec126e212e84ded50bf7dc2861de80def5ec3d33ba8ea1a662733'),
         WeightFile('par_vec_trained_400.pkl.docvecs.vectors_docs.npy',
-                   ('https://dl.dropboxusercontent.com/s/j9gab1u9g5cq91y/par_vec_trained_400_docs.npy?dl=1'),
+                   ('https://dl.dropboxusercontent.com/s/mm0zj6odi2kddht/par_vec_trained_400.pkl.docvecs.vectors_docs.npy?dl=1'),
                    '023dd0b084be9dfa614c27f4b57208d08487174ed5bfd7a27ce604a82ceec797')
     ]
 
