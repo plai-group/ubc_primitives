5cf7d793f73e2bd1040f5838e1a114e98f0fe2d1
tjoseph <tjoseph.ubc@gmail.com>
2020-03-03 15:24:10 -0800

    .

---
+++ primitives/cnn/__init__.py
@@ -0,0 +1,7 @@
+from .cnn import ConvolutionalNeuralNetwork
+
+
+__all__ = ['ConvolutionalNeuralNetwork']
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)  # type: ignore
