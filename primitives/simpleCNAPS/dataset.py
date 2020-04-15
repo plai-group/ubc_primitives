2cca41083bd46f7242fe3831ff24edd255c1c17b
tjoseph <tjoseph.ubc@gmail.com>
2020-04-14 19:29:39 -0700

    updated dataloader

--- primitives/simpleCNAPS/dataset.py
+++ primitives/simpleCNAPS/dataset.py
@@ -5,18 +5,85 @@ from torch.utils import data
 __all__ = ('Dataset')
 
 class Dataset(data.Dataset):
-  # NumPy Dataset
-  def __init__(self, datalist):
-      self.datalist = datalist
+  def __init__(self, datalist, base_dir, mode="Train"):
+      self.base_dir   = base_dir
+      self.datalist   = datalist
+      self._preproces = transforms.Compose([transforms.ToTensor(),
+                                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+      self.__curate_datalist()
+
+  def __curate_datalist(self):
+      # Get set
+      all_data = {}
+      for idx in range(self.datalist.shape[0]):
+          _, _set, img_path, label = list(self.datalist.iloc[idx, :])
+          _set  = int(_set)
+          label = int(label)
+          if _set in all_data.keys():
+              all_data[_set].append([img_path, label])
+          else:
+              all_data[_set] = [[img_path, label]]
+      self.all_data = all_data
+      self.all_set  = sorted(all_data.keys())
+      # Check if set starts with 0 or 1
+      if self.all_set[0] == 1:
+          start_idx = 1
+      else:
+          start_idx = 0
+      self.start_idx  = start_idx
+
+  def __get_task_set(self, index):
+      all_query_set   = []
+      all_support_set = []
+
+      sample_data = self.all_data[index]
+      for data in sample_data:
+          image_path, label = data
+          if "support_set" in image_path:
+              all_support_set.append([image_path, label])
+          elif "query_set" in image_path:
+              all_query_set.append([image_path, label])
+
+      return all_support_set, all_query_set
+
+  def __prepare_task(self, all_support_set, all_query_set):
+      context_images = []
+      context_labels = []
+      target_images  = []
+      target_labels  = []
+
+      # Get support set
+      for supp_set in all_support_set:
+          supp_image_path = os.join.path(base_dir, supp_set[0])
+          supp_image = Image.open(supp_image_path)
+          supp_image = self.pre_process(supp_image)
+          context_images.append(supp_image)
+          context_labels.append(int(supp_set[1]))
+
+      # Get target set
+      for qury_set in all_query_set:
+          qury_image_path = os.join.path(base_dir, qury_set[0])
+          qury_image = Image.open(qury_image_path)
+          qury_image = self.pre_process(qury_image)
+          target_images.append(qury_image)
+          target_labels.append(int(qury_set[1]))
+
+    context_images = torch.FloatTensor(context_images)
+    context_labels = torch.LongTensor(context_labels)
+    target_images  = torch.FloatTensor(target_images)
+    target_labels  = torch.LongTensor(target_labels)
+
+    return context_images, target_images, context_labels, target_labels
 
   def __getitem__(self, index):
-        """
-        Generates one sample of data
-        """
-        sample_data = 0.0
+      """
+      Generates one task sample of data
+      """
+      all_support_set, all_query_set = self.__get_task_set(index=(index+self.start_idx))
+      context_images, target_images, context_labels, target_labels = self.__prepare_task(all_support_set, all_query_set)
 
-        return sample_data
+      return context_images, target_images, context_labels, target_labels
 
   def __len__(self):
-        # Total Number of samples
-        return len(self.all_data)
+      # Total Number of tasks
+      return len(self.all_set)
--- primitives/simpleCNAPS/simple_cnaps.py
+++ primitives/simpleCNAPS/simple_cnaps.py
@@ -5,13 +5,37 @@ from d3m.primitive_interfaces import base
 from d3m.metadata import base as metadata_base, hyperparams, params
 from d3m.primitive_interfaces.supervised_learning import SupervisedLearnerPrimitiveBase
 
-__all__ = ('SimpleCnapsClassifierPrimitive',)
+# Import config file
+from primitives.config_files import config
+
+import logging
+import numpy as np
+from torch.utils import data
+from primitives.simpleCNAPS.dataset import Dataset
+
+__all__ = ('SimpleCNAPSClassifierPrimitive',)
 logger  = logging.getLogger(__name__)
 
 Inputs  = container.DataFrame
 Outputs = container.DataFrame
 
-class SimpleCnapsClassifierPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
+DEBUG = False  # type: ignore
+
+class Params(params.Params):
+    None
+
+class Hyperparams(hyperparams.Hyperparams):
+    """
+    Hyper-parameters for this primitive.
+    """
+    learning_rate = hyperparams.Hyperparameter[float](
+        default=0.0001,
+        description='Learning rate used during training (fit).',
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter'],
+    )
+
+
+class SimpleCNAPSClassifierPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     """
     Simple CNAPS is a simple classcovariance-based distance metric, namely the Mahalanobis
     distance, adopted into a state-of-the-art few-shot learning approach (CNAPS [https://arxiv.org/abs/1906.07697])
@@ -25,11 +49,25 @@ class SimpleCnapsClassifierPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outp
     """
     # Metadata
     __author__ = 'UBC DARPA D3M Team, Tony Joseph <tonyjos@cs.ubc.ca>'
+    global _weights_configs
+    _weights_configs = [{'type': 'FILE',
+                         'key': 'pretrained_resnet.pt.tar',
+                         'file_uri': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+                         'file_digest': '347cb3a744a8ff172f7cc47b4b74987f07ca3b6a1f5d6e4f0037474a38e6b285'},
+                        {'type': 'FILE',
+                         'key': 'best_simple_ar_cnaps.pt',
+                         'file_uri': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+                         'file_digest': '05859f5dec2c70039ff449d37c2474ab07b1a74ab086d2507797d4022d744342'},
+                        {'type': 'FILE',
+                         'key': 'best_simple_cnaps.pt',
+                         'file_uri': 'https://download.pytorch.org/models/googlenet-1378be20.pth',
+                         'file_digest': '79c93169d567ccb50d4303fbd366560effc4d05dfd7e1a4fa7bca0b3dd0c8d6d'},
+    ]
     metadata   =  metadata_base.PrimitiveMetadata({
         "id": "b76a6841-a871-47bc-89ac-e734de5c2924",
         "version": config.VERSION,
-        "name": "Simple CNAPS",
-        "description": "A few shot learning",
+        "name": "Simple CNAPS primitive",
+        "description": "A primitive for few-shot learning task",
         "python_path": "d3m.primitives.classification.simpleCnaps.UBC",
         "primitive_family": metadata_base.PrimitiveFamily.CLASSIFICATION,
         "algorithm_types": [metadata_base.PrimitiveAlgorithmType.NEURAL_NETWORK_BACKPROPAGATION],
@@ -40,7 +78,7 @@ class SimpleCnapsClassifierPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outp
         },
         "keywords": ['neural network', 'few-shot learning', 'deep learning'],
         "installation": [config.INSTALLATION],
-        "hyperparams_to_tune": ['learning_rate', 'optimizer_type']
+        "hyperparams_to_tune": ['learning_rate']
     })
 
 
@@ -51,3 +89,44 @@ class SimpleCnapsClassifierPrimitive(SupervisedLearnerPrimitiveBase[Inputs, Outp
         self._verbose      = _verbose
         self._training_inputs: Inputs   = None
         self._training_outputs: Outputs = None
+        # Is the model fit on the training data
+        self._fitted = False
+
+    def set_training_data(self, *, inputs: Inputs, outputs: Outputs) -> None:
+        self._training_inputs   = inputs
+        self._training_outputs  = outputs
+        self._new_training_data = True
+
+
+    def fit(self, *, timeout: float = None, iterations: int = None) -> base.CallResult[None]:
+        image_columns  = self._training_inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/FileName') # [1]
+        base_paths     = [self._training_inputs.metadata.query((metadata_base.ALL_ELEMENTS, t)) for t in image_columns] # Image Dataset column names
+        base_path      = [base_paths[t]['location_base_uris'][0].replace('file:///', '/') for t in range(len(base_paths))][0] # Path + media
+
+        # Dataset Parameters
+        train_params = {'batch_size': 1,
+                        'shuffle': False,
+                        'num_workers': 4}
+        # DataLoader
+        training_set = Dataset(datalist=self._training_inputs, base_dir=base_path)
+
+        # Data Generators
+        training_generator = data.DataLoader(training_set, **train_params)
+
+        # print(training_set.__getitem__(index=0))
+        # for local_context_images, local_target_images, local_context_labels, local_target_labels in training_generator:
+        #     print(local_context_images.shape)
+        #     break
+
+
+        return base.CallResult(None)
+
+    def produce(self, *, inputs: Inputs, iterations: int = None, timeout: float = None) -> base.CallResult[Outputs]:
+
+        return 0
+
+    def get_params(self) -> Params:
+        return None
+
+    def set_params(self, *, params: Params) -> None:
+        return None
