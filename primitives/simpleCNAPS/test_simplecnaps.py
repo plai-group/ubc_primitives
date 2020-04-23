288bf6c39b6aaf27d683707c0cda4411c7f1f46a
tjoseph <tjoseph.ubc@gmail.com>
2020-04-22 22:51:58 -0700

    added test

--- primitives/simpleCNAPS/test_simplecnaps.py
+++ primitives/simpleCNAPS/test_simplecnaps.py
@@ -72,9 +72,54 @@ class TestSimpleCNAPSClassifierPrimitive(unittest.TestCase):
         cnaps_primitive.fit()
         outputs = cnaps_primitive.produce(inputs=dataframe)
         outputs = outputs.value
+        print(outputs)
+        print(outputs.shape, dataframe.shape)
+
+    def test_1(self):
+        """
+        Dataset test
+        """
+        print('\n')
+        print('########################')
+        print('#--------TEST-3--------#')
+        print('########################')
+
+        # Get volumes:
+        all_weights = os.listdir('./static')
+        all_weights = {w: os.path.join('./static', w) for w in all_weights}
+
+        # Loading dataset.
+        path1 = 'file://{uri}'.format(uri=os.path.abspath('/ubc/cs/research/plai-scratch/tonyj/ubc_primitives/my_datasets/contributed_datasets/LWLL1_metadataset/TEST/dataset_TEST/datasetDoc.json'))
+        dataset = Dataset.load(dataset_uri=path1)
+
+        # Step 0: Denormalize primitive
+        denormalize_hyperparams_class = DenormalizePrimitive.metadata.get_hyperparams()
+        denormalize_primitive = DenormalizePrimitive(hyperparams=denormalize_hyperparams_class.defaults())
+        denormalized_dataset  = denormalize_primitive.produce(inputs=dataset)
+        denormalized_dataset  = denormalized_dataset.value
+        print(denormalized_dataset)
+        print('------------------------')
+
+        print('Loading Training Dataset....')
+        # Step 1: Dataset to DataFrame
+        dataframe_hyperparams_class = DatasetToDataFramePrimitive.metadata.get_hyperparams()
+        dataframe_primitive = DatasetToDataFramePrimitive(hyperparams=dataframe_hyperparams_class.defaults())
+        dataframe = dataframe_primitive.produce(inputs=denormalized_dataset)
+        dataframe = dataframe.value
+        print(dataframe)
+        print('------------------------')
+
+        print('CNAP Primitive....')
+        cnaps_hyperparams_class = SimpleCNAPSClassifierPrimitive.metadata.query()['primitive_code']['class_type_arguments']['Hyperparams']
+        cnaps_hyperparams = cnaps_hyperparams_class.defaults()
+        cnaps_primitive   = SimpleCNAPSClassifierPrimitive(hyperparams=cnaps_hyperparams, volumes=all_weights)
+        cnaps_primitive.set_training_data(inputs=dataframe, outputs=dataframe)
+        cnaps_primitive.fit()
+        outputs = cnaps_primitive.produce(inputs=dataframe)
+        outputs = outputs.value
 
         print(outputs)
-
+        print(outputs.shape, dataframe.shape)
 
 if __name__ == '__main__':
     unittest.main()
